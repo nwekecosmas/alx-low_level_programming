8f0b577b95a23cf61c103653fa89a13e3afaba5b
nwekecosmas <nwekecosmas@gmail.com>
2022-06-30 22:16:55 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * Return: Always 0 (success)
  */
 int main(void)/*Main entry point*/
-}
+{
     puts("\"Programming is like building a multilingual puzzle");
     return (0); /*returning back out of indentation*/
 }
