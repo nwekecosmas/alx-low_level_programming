a6fad41c668940dfc7749a18f505e2d79557ecca
nwekecosmas <nwekecosmas@gmail.com>
2022-06-30 21:18:23 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+#!/bin/bash
 #include <stdio.h>
 /
  * main - Entry point
@@ -5,6 +6,7 @@
  *   */
  int main(void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
