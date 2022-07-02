65d0e5f3eec514a6eeb93d25549868c38b026c3b
nwekecosmas <nwekecosmas@gmail.com>
2022-07-02 04:47:58 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-int i = '0';
-while(i <= '9')
+int i='0';
+while (i <= '9')
 {
 putchar(i); 
 i++;
 }
-i = 'a';
-while(i <= 'f')
+i='a';
+while (i <= 'f')
 {
 putchar(i);
 i++;
 }
 putchar('\n');
-return(0);
+return (0);
 }
