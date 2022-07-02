fccd8338148406a03b722fb6be76e82aceb9dfa5
nwekecosmas <nwekecosmas@gmail.com>
2022-07-02 06:12:11 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ char a;
 int i;
 long int li;
 long long int lli;
-float f;	
+float f;
 printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
