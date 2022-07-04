45f1c4ae71ccd8f463b720b57f462bf97c432a2c
nwekecosmas <nwekecosmas@gmail.com>
2022-07-03 21:55:24 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,10 +13,6 @@ long l = 59;
 long fd = 1;
 long syscall = 1;
 long ret = 0;
-_asm_ ("syscall"
-: "=a" (ret)
-: "a" (syscall),
-"D" (fd),
-"S" (s),
-"d" (l)); return (1);
+_asm_ ("syscall": "=a" (ret): "a" (syscall),"D" (fd),"S" (s),"d" (l));
+return (1);
 }
