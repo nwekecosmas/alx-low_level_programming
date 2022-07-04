11669a8a1af42b16fe43ced92faa01c336fc6ab6
nwekecosmas <nwekecosmas@gmail.com>
2022-07-03 21:51:16 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
-/*
-this is my program
-*/
+/**
+ * main - prints to string
+ *
+ * Description: Prints "and that piece of art is useful..." without puts
+ *
+ * Return: 1
+ */
 int main(void)
 {
-printf("and that piece of art is useful" - Dora Korpar, 2015-10-19");
-return (1);
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 59;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+_asm_ ("syscall"
+: "=a" (ret)
+: "a" (syscall),
+"D" (fd),
+"S" (s),
+"d" (l)); return (1);
 }
