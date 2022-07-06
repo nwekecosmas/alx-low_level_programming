31fa03d7206e0591494d5a36cdfb388cea569e11
nwekecosmas <nwekecosmas@gmail.com>
2022-07-06 02:11:10 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,15 +9,17 @@ void times_table(void)
 {
 int a, b, res;
 for (a = 0; a <= 9; a++)
-{
+{    
 for (b = 0; b <= 9; b++)
 {
 res = (a * b);
 if (b != 0)
 {
 _putchar(',');
-_putchar(' ');	      
-     }
+	      
+	   _putchar(' ');
+	      
+	    }
 	  
 	  if (res >= 10)
 	    
