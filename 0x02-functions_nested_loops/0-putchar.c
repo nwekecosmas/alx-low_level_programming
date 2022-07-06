ace5ad6570fdd04c8458b60d55a64e86bef49c32
nwekecosmas <nwekecosmas@gmail.com>
2022-07-05 20:35:38 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,12 @@
 #include "main.h"
+
 /**
-
  * main - prints a string of characters
-
  * Description: prints _putchar, followed by a new line
-
  * Return: 0
-
  */
 int main(void)
-{  
+{
 char a[] = "_putchar";
 int i;
 for (i = 0; i < 8; i++)
