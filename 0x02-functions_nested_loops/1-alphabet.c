6f911e2e4c303bb1f41cfe40e453048289e55873
nwekecosmas <nwekecosmas@gmail.com>
2022-07-05 20:58:39 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabet
+ * Description: printing alphabets in lowercase
+ *Return: void
+ */
+void print_alphabet(void)
+{
+char s;  
+for (s = 'a'; s  <= 'z'; s++)
+{
+_putchar(s);
+}
+_putchar('\n');
+}
