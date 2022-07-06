1c1cb9615ca604a627d348275a40d22f6d4f8646
nwekecosmas <nwekecosmas@gmail.com>
2022-07-05 21:57:54 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,14 @@
  */
 void print_alphabet_x10(void)
 {
-int i;  
-char m;  
-for (i = 0; i < 10; ++i)    
+int i;
+char m;
+for (i = 0; i < 10; ++i)
 {  
 for (m = 'a'; m <= 'z'; m++)
 {
 _putchar(m);
-}    
-_putchar('\n');      
-}  
+}
+_putchar('\n');
+}
 }
